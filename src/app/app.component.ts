8c984bbdc54e158b819feabde895e1ff6ae890f7
Rehan Ayoub <rehan.ayoub@ttu.edu>
2018-06-13 14:51:19 -0500

    angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of heroes'; //try adding more configuration
+  
 }
+//delete the url in app.component.html
